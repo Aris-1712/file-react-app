4cd9d218569fc290dbedcbfa1af95d00d753d242
Aris <aris@Ariss-MacBook-Air.local>
2022-01-14 12:15:35 +0400

    published

--- src/Views/Header/index.js
+++ src/Views/Header/index.js
@@ -6,7 +6,9 @@ const Header=(props)=>{
         <div className='container-fluid p-5'>
             <div className='d-flex flex-row justify-content-center'>
                 <h1>Files App</h1>
+                
             </div>  
+            <div className='d-flex flex-row justify-content-center mt-2'><p>(Double click on file or folder to access)</p></div>
 
         </div>
     )
